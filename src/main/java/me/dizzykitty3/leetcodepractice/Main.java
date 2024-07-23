01727a9949f1216eb347c4ab6e61b60a0743c725
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2024-07-23 14:14:50 +0800

    .

--- src/main/java/me/dizzykitty3/leetcodepractice/Main.java
+++ src/main/java/me/dizzykitty3/leetcodepractice/Main.java
@@ -6,4 +6,27 @@ public class Main {
         System.out.println("CLICK THE LINK TO START: 👇");
         System.out.println("https://leetcode.com/problemset/all/");
     }
+
+    // Note:
+    // If you are unable to run this project, please consider setting up a proxy for Maven.
+    // 1. Create a file named settings.xml at C:\Users\<your_username>\.m2\
+    // 2. Open it and fill in the proxy settings. for example:
+    // ---------- starts here ----------
+    //<settings>
+    //  .
+    //  <proxies>
+    //   <proxy>
+    //      <id>example-proxy</id>
+    //      <active>true</active>
+    //      <protocol>http</protocol>
+    //      <host>127.0.0.1</host>
+    //      <port>7890</port>
+    //      <username></username>
+    //      <password></password>
+    //      <nonProxyHosts></nonProxyHosts>
+    //    </proxy>
+    //  </proxies>
+    //  .
+    //</settings>
+    // ---------- ends here ----------
 }
