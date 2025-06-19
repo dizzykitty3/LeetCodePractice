3ccde81d4bec9f5353e6f1271b77380e2e366ab6
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2025-06-19 15:12:36 +0800

    2294 Partition Array Such That Maximum Difference Is K

---
+++ src/main/java/me/dizzykitty3/leetcodepractice/difficulty_2_medium/Solution2294.java
@@ -0,0 +1,33 @@
+package me.dizzykitty3.leetcodepractice.difficulty_2_medium;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * 2294 Partition Array Such That Maximum Difference Is K
+ */
+public class Solution2294 {
+    public int partitionArray(int[] nums, int k) {
+        List<Integer> list = Arrays.stream(nums).boxed().sorted().toList();
+        var result = 0;
+        var tmp = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) - tmp > k) {
+                result++;
+                tmp = list.get(i);
+            }
+        }
+        return result;
+    }
+
+    @Test
+    void testPartitionArray() {
+        assertEquals(2, partitionArray(new int[]{3, 6, 1, 2, 5}, 2));
+        assertEquals(2, partitionArray(new int[]{1, 2, 3}, 1));
+        assertEquals(3, partitionArray(new int[]{2, 2, 4, 5}, 0));
+    }
+}
