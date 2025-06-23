a5f893f919721204a01843b3cec728953a228fcb
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2025-06-23 16:18:20 +0800

    2 Add Two Numbers

--- src/main/java/me/dizzykitty3/leetcodepractice/ListNode.java
+++ src/main/java/me/dizzykitty3/leetcodepractice/ListNode.java
@@ -1,21 +1,51 @@
 package me.dizzykitty3.leetcodepractice;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Definition for singly-linked list.
  */
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
-    ListNode() {
+    public ListNode() {
     }
 
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
 
-    ListNode(int val, ListNode next) {
+    public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
+    }
+
+    public ListNode(int[] vals) {
+        this.val = vals[0];
+
+        var length = vals.length;
+        ListNode temp = null;
+        for (int i = 1; i < length; i++) {
+            var currentNode = new ListNode(vals[i]);
+            if (i == 1) {
+                this.next = currentNode;
+                temp = currentNode;
+            } else {
+                temp.next = currentNode;
+                temp = currentNode;
+            }
+        }
+    }
+
+    public static Object[] toArray(ListNode listNode) {
+        var list = new ArrayList<Integer>(Collections.emptyList());
+        list.add(listNode.val);
+        while (listNode.next != null) {
+            list.add(listNode.next.val);
+            listNode = listNode.next;
+        }
+        return list.toArray();
     }
 }
---
+++ src/main/java/me/dizzykitty3/leetcodepractice/difficulty_2_medium/Solution2.java
@@ -0,0 +1,84 @@
+package me.dizzykitty3.leetcodepractice.difficulty_2_medium;
+
+import me.dizzykitty3.leetcodepractice.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static me.dizzykitty3.leetcodepractice.ListNode.toArray;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+/**
+ * 2 Add Two Numbers
+ */
+public class Solution2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result;
+        var needPlusOne = false;
+        if (l1.val + l2.val == 10) {
+            result = new ListNode(0);
+            needPlusOne = true;
+        } else {
+            result = new ListNode(l1.val + l2.val);
+        }
+        var temp = result;
+
+        while (l1.next != null || l2.next != null) {
+            if (l1.next != null && l2.next == null) {
+                if (l1.next.val == 9 && needPlusOne) {
+                    temp.next = new ListNode(0);
+                } else if (needPlusOne) {
+                    temp.next = new ListNode(l1.next.val + 1);
+                    needPlusOne = false;
+                } else {
+                    temp.next = new ListNode(l1.next.val);
+                }
+                l1 = l1.next;
+            } else if (l1.next == null) {
+                if (l2.next.val == 9 && needPlusOne) {
+                    temp.next = new ListNode(0);
+                } else if (needPlusOne) {
+                    temp.next = new ListNode(l2.next.val + 1);
+                    needPlusOne = false;
+                } else {
+                    temp.next = new ListNode(l2.next.val);
+                }
+                l2 = l2.next;
+            } else {
+                if ((l1.next.val + l2.next.val == 9 && needPlusOne) || (l1.next.val + l2.next.val == 10)) {
+                    temp.next = new ListNode(0);
+                    needPlusOne = true;
+                } else if (needPlusOne) {
+                    temp.next = new ListNode(l1.next.val + l2.next.val + 1);
+                    needPlusOne = false;
+                } else {
+                    temp.next = new ListNode(l1.next.val + l2.next.val);
+                }
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        if (needPlusOne) {
+            temp.next = new ListNode(1);
+        }
+        return result;
+    }
+
+    @Test
+    void testToArray() {
+        assertArrayEquals(new Object[]{1, 2, 3}, toArray(new ListNode(new int[]{1, 2, 3})));
+        assertArrayEquals(new Object[]{4, 5, 6}, toArray(new ListNode(new int[]{4, 5, 6})));
+        assertArrayEquals(new Object[]{4, 5, 6, 7}, toArray(new ListNode(new int[]{4, 5, 6, 7})));
+        assertArrayEquals(new Object[]{0}, toArray(new ListNode(new int[]{0})));
+    }
+
+    @Test
+    void testAddTwoNumbers() {
+        assertArrayEquals(toArray(new ListNode(new int[]{7, 9, 7})), toArray(addTwoNumbers(new ListNode(new int[]{2, 3, 3}), new ListNode(new int[]{5, 6, 4}))));
+        assertArrayEquals(toArray(new ListNode(new int[]{7, 0, 8})), toArray(addTwoNumbers(new ListNode(new int[]{2, 3, 3}), new ListNode(new int[]{5, 7, 4}))));
+        assertArrayEquals(toArray(new ListNode(new int[]{7, 0, 8})), toArray(addTwoNumbers(new ListNode(new int[]{2, 4, 3}), new ListNode(new int[]{5, 6, 4}))));
+        assertArrayEquals(toArray(new ListNode(new int[]{0})), toArray(addTwoNumbers(new ListNode(new int[]{0}), new ListNode(new int[]{0}))));
+        assertArrayEquals(toArray(new ListNode(new int[]{0, 1})), toArray(addTwoNumbers(new ListNode(new int[]{4}), new ListNode(new int[]{6}))));
+        assertArrayEquals(toArray(new ListNode(new int[]{0, 1, 0, 1})), toArray(addTwoNumbers(new ListNode(new int[]{1, 0, 1}), new ListNode(new int[]{9, 0, 9}))));
+        assertArrayEquals(toArray(new ListNode(new int[]{8, 9, 9, 9, 0, 0, 0, 1})), toArray(addTwoNumbers(new ListNode(new int[]{9, 9, 9, 9, 9, 9, 9}), new ListNode(new int[]{9, 9, 9, 9}))));
+    }
+}
